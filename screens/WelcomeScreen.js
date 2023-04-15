b35eafe89e9f0f5054749b6bbbc2d371efced374
Isis Najera <isisnajer@gmail.com>
2023-04-15 09:04:36 -0700

    Login Frame/Sign In

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -8,9 +8,9 @@ function WelcomeScreen(props) {
             source={require("../assets/homescreen.jpg")}
         >
             <Image style={styles.logo} source={require("../assets/logo.png")}/>
-            {/* <Text>Trade What You Don't Need</Text> */}
             <View style={styles.loginButton}></View>
             <View style={styles.registerButton}></View>
+            <Text style={styles.textBottom}>Already have an account? Sign In</Text>
         </ImageBackground>
     );
 }
@@ -21,22 +21,35 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "center",
     },
-    // loginButton: {
-    //     width: '100%',
-    //     height: 70,
-    //     backgroundColor: "#C3B19B",
-    // },
+    loginButton: {
+        width: '55%',
+        height: 70,
+        backgroundColor: "#A9C0D6",
+        opacity: "0.8",
+        borderRadius: "20%",
+        marginBottom: "19%"
+    },
     // registerButton: {
-    //     width: '100%',
+    //     width: '55%',
     //     height: 70,
-    //     backgroundColor: "#E3DFDA",
+    //     backgroundColor: "#A9C0D6",
+    //     opacity: "0.7",
+    //     borderRadius: "18%",
+    //     marginBottom: "3%"
     // },
     logo: {
         width: 440,
         height: 460,
         position: 'absolute',
-        top: 180,
+        top: 175,
     },
+    textBottom: {
+        alignContent: "center",
+        fontSize: 13,
+        color: "#FFDE59",
+        marginBottom: "10%",
+        fontWeight: "700",
+    }
 })
 
 export default WelcomeScreen;
